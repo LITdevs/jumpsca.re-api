2d508b1126f2b78858bd290176553ec1242760c1
Amy <amy@litdevs.org>
2023-07-23 17:21:17 +0300

    Attempt to fix parameter in post-deploy

--- yggdrasil.config.cjs
+++ yggdrasil.config.cjs
@@ -22,7 +22,7 @@ module.exports = {
 			"ref"  : "origin/phoenix",
 			"repo" : "git@github.com:LITdevs/jumpsca.re-api.git",
 			"path" : "/home/jumpscare/phoenix",
-			"post-deploy" : "yarn install && pm2 startOrRestart yggdrasil.config.cjs --env phoenix --only \"jumpsca.re-phoenix\""
+			"post-deploy" : "yarn install && pm2 startOrRestart yggdrasil.config.cjs --only jumpsca.re-phoenix --env phoenix"
 		}
 	}
 };
