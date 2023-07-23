29819eba0b153642c4bcfdb741480ef1bb4fbcfc
Amy <amy@litdevs.org>
2023-07-23 16:53:15 +0300

    Add experimental pm2 config for deployments

---
+++ yggdrasil.config.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+	apps : [{
+		script: 'npm run start',
+	}],
+	// Deployment Configuration
+	deploy : {
+		phoenix : {
+			"user" : "jumpscare",
+			"host" : ["kirito.yggdrasil.cat"],
+			"ref"  : "origin/phoenix",
+			"repo" : "git@github.com:LITdevs/jumpsca.re-api.git",
+			"path" : "/home/jumpscare/phoenix",
+			"post-deploy" : "yarn install"
+		}
+	},
+	env: {
+		NODE_ENV: 'development',
+		DEBUG: 'custom:*'
+	}
+};
