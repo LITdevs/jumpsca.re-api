fa2b884591456a715469862c9f90fcdd44290365
Emilia <emilia@jumpsca.re>
2025-01-18 18:44:38 +0200

    Support custom headers and redirects

--- src/routes/v1/site.ts
+++ src/routes/v1/site.ts
@@ -12,13 +12,15 @@ router.post('/webhook/:siteId', async (req, res) => {
     let site = await database.Site.findOne({secret: req.query?.key, siteId: req.params.siteId});
     if (!site) return res.reply(new NotFoundReply())
     // noinspection HttpUrlsUsage
-    let response = await axios.post(`http://asuna.yggdrasil.cat:5000/deploy/${req.params.siteId}`, JSON.stringify({
+    let response = await axios.post(`http://asuna.vps.yggdrasil.cat:5000/deploy/${req.params.siteId}`, JSON.stringify({
         domains: site.deployConfig.domains,
         repoUrl: req.body.repository.url,
         serveFolder: site.deployConfig.serveFolder,
         branch: site.deployConfig.branch,
         environment: site.deployConfig.environment,
-        buildCommand: site.deployConfig.buildCommand
+        buildCommand: site.deployConfig.buildCommand,
+        headers: site.deployConfig.headers,
+        redirects: site.deployConfig.redirects
     }), {
         headers: {
             "Content-Type": "application/json",
